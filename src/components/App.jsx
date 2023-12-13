faa41a1aa7198a37ee033c80e7dc5f3ea56119bf
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-12-13 12:29:58 +0200

    hw 1-friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import user from '../data/user.json';
 import data from '../data/data.json';
+import friends from '../data/friends.json';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
 
 export const App = () => {
   return (
@@ -14,6 +16,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics stats={data} />
+      <FriendList friends={friends} />;
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,19 @@
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(friend => (
+        <FriendItem key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+};
+
+const FriendItem = ({ friend: { avatar, name, isOnline } }) => {
+  return (
+    <li className="item">
+      <span className="status">{isOnline}</span>
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <p className="name">{name}</p>
+    </li>
+  );
+};
