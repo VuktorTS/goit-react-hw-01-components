e48e3741e96521ef3b3d58c8f0ad2d7de9dc5987
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-12-14 16:49:09 +0200

    add style for friendList

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,6 +1,8 @@
+import css from './FriendList.module.css';
+
 export const FriendList = ({ friends }) => {
   return (
-    <ul className="friend-list">
+    <ul className={css.friend_list}>
       {friends.map(friend => (
         <FriendItem key={friend.id} friend={friend} />
       ))}
@@ -10,10 +12,13 @@ export const FriendList = ({ friends }) => {
 
 const FriendItem = ({ friend: { avatar, name, isOnline } }) => {
   return (
-    <li className="item">
-      <span className="status">{'a ' + isOnline}</span>
-      <img className="avatar" src={avatar} alt="User avatar" width="48" />
-      <p className="name">{name}</p>
+    <li className={css.item}>
+      <span
+        className={css.status}
+        style={{ backgroundColor: isOnline ? 'green' : 'red' }}
+      ></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
     </li>
   );
 };
